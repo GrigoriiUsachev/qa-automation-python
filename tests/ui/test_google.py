ff8696f1ead7da6adb977c4f3804925f7de651d0
GrigoriiUsachev <usachevgrigoriy@gmail.com>
2025-12-29 00:16:25 -0500

    Fix CI: run only API tests

--- tests/ui/test_google.py
+++ tests/ui/test_google.py
@@ -1,11 +1,11 @@
-import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 from pages.login_page import LoginPage
 
-pytestmark = pytest.mark.ui
+
 
 
 @pytest.fixture
